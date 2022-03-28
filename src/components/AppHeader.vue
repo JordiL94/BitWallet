a4126cc21feb958b6b73a8c9388e2f6669e055bb
Mansei <yarden835@gmail.com>
2022-03-28 21:28:16 +0300

    header fixes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-	<section v-if="user" class="app-header flex space-between main-layout">
+	<section class="app-header flex space-between main-layout">
 		<!-- <img src="../assets/img/logo.png" alt="" /> -->
 		<RouterLink to="/">
 			<h3>
@@ -9,7 +9,6 @@
 		</RouterLink>
 		<RouterLink to="/contact">Contacts</RouterLink>
 	</section>
-	<h3 v-else>BitWallet</h3>
 </template>
 
 <script>
