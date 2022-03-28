6972d6aa42be7d1cdced9aad75c0b4361b72422b
Mansei <yarden835@gmail.com>
2022-03-28 10:15:59 +0300

    design changes

--- src/components/contact/ContactList.vue
+++ src/components/contact/ContactList.vue
@@ -1,21 +1,26 @@
 <template>
-  <section class="contact-list">
-      <ul class="clean-list flex column justify-center">
-          <li v-for="contact in contacts" :key="contact._id" contact="contact-preview-container flex">
-              <ContactPreview :contact="contact" />
-          </li>
-      </ul>
-  </section>
+	<section class="contact-list">
+		<ul class="clean-list flex column justify-center">
+			<li
+				v-for="contact in contacts"
+				:key="contact._id"
+				contact="contact-preview-container flex"
+			>
+				<ContactPreview :contact="contact" />
+			</li>
+		</ul>
+		<RouterLink to="/contact/edit" class="add-contact">+</RouterLink>
+	</section>
 </template>
 
 <script>
-import ContactPreview from './ContactPreview.vue';
-export default {
-    props: {
-        contacts: Array
-    },
-    components: {
-        ContactPreview
-    },
-}
+	import ContactPreview from './ContactPreview.vue';
+	export default {
+		props: {
+			contacts: Array,
+		},
+		components: {
+			ContactPreview,
+		},
+	};
 </script>
